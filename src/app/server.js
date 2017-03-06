540d2b90f3b9fc7b15f4f979a34f95395ae6161c
brianhong <brianhong0510@gmail.com>
2017-03-06 17:21:46 -0500

    Added hero images

--- src/app/server.js
+++ src/app/server.js
@@ -7,10 +7,13 @@ const port = 3000
 const my_key = 'B569FF6CF888AB6CB16083BF33A917E3';
 
 const basePath = 'http://api.steampowered.com/'
+const getMatchHistoryPath = "/IDOTA2Match_570/GetMatchHistory/v1/"
 
-app.get('/', (req, res) => {
-  request(basePath + '/IDOTA2Match_570/GetMatchHistory/V001/?matches_requested=2&key=' + my_key, function(error, response, body){
-    if(!error && response.statusCode == 200) {
+app.get('/matches', (req, res) => {
+  console.log('GET-ting matches...')
+  request(basePath + getMatchHistoryPath + '?matches_requested=10&game_mode=1&min_players=10&key=' + my_key, (error, response, body) => {
+    var obj_body = JSON.parse(body)
+    if(!error && response.statusCode == 200 && obj_body['result']['status'] === 1) {
       fs.writeFile('test.txt', body, (err) => {
         if (err) {
           return console.log(err)
@@ -18,7 +21,7 @@ app.get('/', (req, res) => {
       })
     }
   })
-  console.log("I come first")
+  res.send('Working on it...')
 })
 
 app.listen(port, (err) => {
